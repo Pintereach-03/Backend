04eaea0fdf1d621bd7ee8e72b142e5c13ba00d38
Hugo-Orozco <aceasin@outlook.com>
2021-06-21 09:20:24 -0600

    Production Build

--- knexfile.js
+++ knexfile.js
@@ -2,17 +2,22 @@ const sharedConfig = {
     client: 'sqlite3',
     useNullAsDefault: true,
     migrations: { directory: './data/migrations' },
-    pool: { afterCreate: (conn, done) => conn.run('PRAGMA foreign_keys = ON', done) },
-  }
+    pool: { afterCreate: (conn, done) => conn.run('PRAGMA foreign_keys = ON', done) }
+};
 
-  module.exports = {
+module.exports = {
     development: {
-      ...sharedConfig,
-      connection: { filename: './data/auth.db3' },
-      seeds: { directory: './data/seeds' },
+        ...sharedConfig,
+        connection: { filename: './data/auth.db3' },
+        seeds: { directory: './data/seeds' }
     },
     testing: {
-      ...sharedConfig,
-      connection: { filename: './data/test.db3' },
+        ...sharedConfig,
+        connection: { filename: './data/test.db3' }
     },
-  };
+    production: {
+        ...sharedConfig,
+        connection: { filename: './data/auth.db3' },
+        seeds: { directory: './data/seeds' }
+    }
+};
